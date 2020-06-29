ab625fb8cce73699d61634cd1a8a2dcfc2a76342
HELFER Thomas 202608 <thomas.helfer@cea.fr>
2020-06-29 14:24:07 +0200

    validate for merge

---
+++ bindings/python/src/api/api_local.cxx
@@ -0,0 +1,15 @@
+/*!
+ * \file   bindings/python/src/api_local.cxx
+ * \brief    
+ * \author Helfer Thomas
+ * \date   31 janv. 2017
+ */
+
+#include<boost/python.hpp>
+
+void declareFile();
+
+BOOST_PYTHON_MODULE(api_local)
+{
+  declareFile();
+}
